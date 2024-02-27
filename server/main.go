67dc86f9715664a388be2442ec3af05b0d2b3439
Andrejs Sikorskis <as283@alu.ua.es>
2024-02-27 13:26:33 +0100

    servidor to wapo

--- server/main.go
+++ server/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		println("Hello, Server!")
+	http.HandleFunc("/", func(writer http.ResponseWriter, req *http.Request) {
+		writer.Write([]byte(`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Document</title>
+		</head>
+		<body>
+			<h1>Hello, Server!</h1>
+		</body>
+		</html>`))
+		fmt.Printf("Request received from %s\n", req.RemoteAddr)
 	})
 
+	fmt.Println("Your server is running on https://localhost:10443")
 	err := http.ListenAndServeTLS(":10443", "localhost.crt", "localhost.key", nil)
 
 	if err != nil {
+		fmt.Println("Your server is not running on https://localhost:10443")
 		println("ListenAndServeTLS:", err.Error())
 	}
 }
