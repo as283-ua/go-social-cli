e0f98618d4982cce05e135c1ea8aae38b8c090d1
aic32-ua <aic32@alu.ua.es>
2024-03-05 12:12:12 +0100

    Improvements

--- server/main.go
+++ server/main.go
@@ -3,7 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type user struct {
+	Name  string            // nombre de usuario
+	Hash  []byte            // hash de la contraseña
+	Salt  []byte            // sal para la contraseña
+	Token []byte            // token de sesión
+	Seen  time.Time         // última vez que fue visto
+	Data  map[string]string // datos adicionales del usuario
+}
+
+var gUsers map[string]user
+
+func chk(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, req *http.Request) {
@@ -24,8 +42,5 @@ func main() {
 	fmt.Println("Your server is running on https://localhost:10443")
 	err := http.ListenAndServeTLS(":10443", "localhost.crt", "localhost.key", nil)
 
-	if err != nil {
-		fmt.Println("Your server is not running on https://localhost:10443")
-		println("ListenAndServeTLS:", err.Error())
-	}
+	chk(err)
 }
