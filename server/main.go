6f395d705c641a2cccc91239185849e7d09b2a0c
aic32-ua <aic32@alu.ua.es>
2024-03-05 12:30:18 +0100

    More improvements

--- server/main.go
+++ server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"crypto/rand"
+	"encoding/json"
 	"io"
 	"log/slog"
 	"net/http"
 	"time"
+	"util"
+
+	"golang.org/x/crypto/scrypt"
 )
 
 type user struct {
@@ -21,6 +26,7 @@ var gUsers map[string]user
 
 func chk(e error) {
 	if e != nil {
+		logger.Error(e.Error())
 		panic(e)
 	}
 }
@@ -42,32 +48,92 @@ func main() {
 
 func handle(writer http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
-	if err != nil {
-		logger.Error("Error parsing form data:", err)
-		writer.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	writer.Header().Set("Content-Type", "text/plain")
+	chk(err)
 
-	writer.Write([]byte(`<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-	</head>
-	<body>
-		<h1>Hello, Server!</h1>
-	</body>
-	</html>`))
-	bodyStr, err := io.ReadAll(req.Body)
-	if err != nil {
-		logger.Error("Error reading body:", err)
-		writer.WriteHeader(http.StatusInternalServerError)
-		return
+	switch req.Form.Get("cmd") { // comprobamos comando desde el cliente
+	case "register": // ** registro
+		_, ok := gUsers[req.Form.Get("user")] // ¿existe ya el usuario?
+		if ok {
+			response(writer, false, "Usuario ya registrado", nil)
+			return
+		}
+
+		u := user{}
+		u.Name = req.Form.Get("user")                   // nombre
+		u.Salt = make([]byte, 16)                       // sal (16 bytes == 128 bits)
+		rand.Read(u.Salt)                               // la sal es aleatoria
+		u.Data = make(map[string]string)                // reservamos mapa de datos de usuario
+		u.Data["private"] = req.Form.Get("prikey")      // clave privada
+		u.Data["public"] = req.Form.Get("pubkey")       // clave pública
+		password := util.Decode64(req.Form.Get("pass")) // contraseña (keyLogin)
+
+		// "hasheamos" la contraseña con scrypt (argon2 es mejor)
+		u.Hash, _ = scrypt.Key(password, u.Salt, 16384, 8, 1, 32)
+
+		u.Seen = time.Now()        // asignamos tiempo de login
+		u.Token = make([]byte, 16) // token (16 bytes == 128 bits)
+		rand.Read(u.Token)         // el token es aleatorio
+
+		gUsers[u.Name] = u
+		response(writer, true, "Usuario registrado", u.Token)
+
+	case "login": // ** login
+		u, ok := gUsers[req.Form.Get("user")] // ¿existe ya el usuario?
+		if !ok {
+			response(writer, false, "Usuario inexistente", nil)
+			return
+		}
+
+		password := util.Decode64(req.Form.Get("pass"))          // obtenemos la contraseña (keyLogin)
+		hash, _ := scrypt.Key(password, u.Salt, 16384, 8, 1, 32) // scrypt de keyLogin (argon2 es mejor)
+		if !bytes.Equal(u.Hash, hash) {                          // comparamos
+			response(writer, false, "Credenciales inválidas", nil)
+
+		} else {
+			u.Seen = time.Now()        // asignamos tiempo de login
+			u.Token = make([]byte, 16) // token (16 bytes == 128 bits)
+			rand.Read(u.Token)         // el token es aleatorio
+			gUsers[u.Name] = u
+			response(writer, true, "Credenciales válidas", u.Token)
+		}
+
+	case "data": // ** obtener datos de usuario
+		u, ok := gUsers[req.Form.Get("user")] // ¿existe ya el usuario?
+		if !ok {
+			response(writer, false, "No autentificado", nil)
+			return
+		} else if (u.Token == nil) || (time.Since(u.Seen).Minutes() > 60) {
+			// sin token o con token expirado
+			response(writer, false, "No autentificado", nil)
+			return
+		} else if !bytes.EqualFold(u.Token, util.Decode64(req.Form.Get("token"))) {
+			// token no coincide
+			response(writer, false, "No autentificado", nil)
+			return
+		}
+
+		datos, err := json.Marshal(&u.Data) //
+		chk(err)
+		u.Seen = time.Now()
+		gUsers[u.Name] = u
+		response(writer, true, string(datos), u.Token)
+
+	default:
+		response(writer, false, "Comando no implementado", nil)
 	}
-	fmt.Println(string(bodyStr))
-	logger.Info("Header", "header", req.Header)
-	logger.Info("Request", "method", req.Method, "url", req.URL)
-	logger.Info("Form", "form", req.Form)
-	fmt.Println()
+}
+
+type Resp struct {
+	Ok    bool   // true -> correcto, false -> error
+	Msg   string // mensaje adicional
+	Token []byte // token de sesión para utilizar por el cliente
+}
+
+// función para escribir una respuesta del servidor
+func response(w io.Writer, ok bool, msg string, token []byte) {
+	r := Resp{Ok: ok, Msg: msg, Token: token} // formateamos respuesta
+	rJSON, err := json.Marshal(&r)            // codificamos en JSON
+	chk(err)                                  // comprobamos error
+	w.Write(rJSON)                            // escribimos el JSON resultante
 }
